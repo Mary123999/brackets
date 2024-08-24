80f8dd720c3ea7a20b10b746a26e61818af385b1
Mary123999 <mgordienko552@gmail.com>
2024-08-24 14:13:50 +0300

    feat: update index.js  file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+const lifo = [];
+const open = bracketsConfig.map(pair=> pair[0]);
+const close = bracketsConfig.map(pair=> pair[1]);
+const match = Object.fromEntries(bracketsConfig);
+for(let symvol of str){
+  if(open.includes(symvol)){
+    if (close.includes(symvol)&& lifo[lifo.length-1]===symvol){
+      lifo.pop();
+    }
+    else{
+      lifo.push(symvol);
+    }
+    }
+    else{
+      if(lifo.length===0 || match[lifo.pop()] !==symvol){
+        return false;
+      }
+    }
+   
+  }
+  return lifo.length===0;
 }
+
